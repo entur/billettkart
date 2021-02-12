159ed54d61fce1225f8fa6f1822806d62eed8c50
Mats Byrkjeland <mats.byrkjeland@entur.org>
2021-02-12 14:08:43 +0100

    Setup initial map with layer toggles

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,80 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
+import ReactMapGL, { MapRef } from 'react-map-gl'
+import { Layer } from 'mapbox-gl'
 
-import { Heading1, Paragraph } from '@entur/typography'
+import { Checkbox, Fieldset } from '@entur/form'
+import { Contrast } from '@entur/layout'
+import { Heading1 } from '@entur/typography'
 
-const Home: React.FC = () => (
-    <div>
-        <Heading1>Entur samler kollektiv-Norge</Heading1>
-        <Paragraph>Sammen kommer vi lenger</Paragraph>
-    </div>
-)
+import './styles.css'
+
+const Home: React.FC = () => {
+    const mapRef = useRef<MapRef>()
+
+    const [viewport, setViewport] = useState<any>({
+        width: window.innerWidth,
+        height: window.innerHeight,
+        latitude: 64,
+        longitude: 16,
+        zoom: 4,
+    })
+
+    const [layers, setLayers] = useState<Layer[]>([])
+    const [activeLayers, setActiveLayers] = useState<string[]>([])
+
+    const onLoad = () => {
+        const map = mapRef.current?.getMap()
+        if (!map) return
+        const style = map.getStyle()
+        const allLayers = (style.layers as Layer[]).filter(
+            (layer) => layer.id !== 'background',
+        )
+        setLayers(allLayers)
+        setActiveLayers(allLayers.map(({ id }) => id))
+    }
+
+    const setLayerVisibility = (layer: Layer, visible: boolean): void => {
+        const map = mapRef.current?.getMap()
+        if (!map) return
+        if (visible) {
+            setActiveLayers((prev) => [...prev, layer.id])
+            map.addLayer(layer)
+        } else {
+            map.removeLayer(layer.id)
+            setActiveLayers((prev) => prev.filter((id) => id !== layer.id))
+        }
+    }
+
+    return (
+        <>
+            <ReactMapGL
+                ref={mapRef}
+                onLoad={onLoad}
+                {...viewport}
+                mapboxApiAccessToken={process.env.MAPBOX_TOKEN}
+                mapStyle={process.env.MAPBOX_STYLE}
+                onViewportChange={(nextViewport: any) =>
+                    setViewport(nextViewport)
+                }
+            />
+            <Contrast className="control-panel">
+                <Heading1>Tilbudskart</Heading1>
+                <Fieldset label="Hva vil du se?">
+                    {layers.map((layer) => (
+                        <Checkbox
+                            checked={activeLayers.includes(layer.id)}
+                            onChange={(e) =>
+                                setLayerVisibility(layer, e.target.checked)
+                            }
+                            key={layer.id}
+                        >
+                            {layer.id}
+                        </Checkbox>
+                    ))}
+                </Fieldset>
+            </Contrast>
+        </>
+    )
+}
 
 export default Home
