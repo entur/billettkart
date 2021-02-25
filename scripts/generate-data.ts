f33d4ac0e581686b721f051cfe33a55b8ae6dbfe
Mats Byrkjeland <mats.byrkjeland@entur.org>
2021-02-25 19:38:06 +0100

    Add generation of trafikkpakke data

--- scripts/generate-data.ts
+++ scripts/generate-data.ts
@@ -1,9 +1,30 @@
 import fs from 'fs'
 import util from 'util'
-import { Feature, FeatureCollection, MultiPolygon, Polygon } from 'geojson'
+import {
+    Feature,
+    FeatureCollection,
+    MultiLineString,
+    MultiPolygon,
+    Polygon,
+} from 'geojson'
 
 const readFile = util.promisify(fs.readFile)
 const writeFile = util.promisify(fs.writeFile)
+
+const readJson = async <T>(path: string): Promise<T> => {
+    const contents = await readFile(path, { encoding: 'utf-8' })
+    return JSON.parse(contents)
+}
+
+const writeJson = async <T>(path: string, data: T): Promise<void> => {
+    await writeFile(path, JSON.stringify(data), {
+        encoding: 'utf-8',
+    })
+}
+
+function featureCollection(features: Feature[]): FeatureCollection {
+    return { type: 'FeatureCollection', features }
+}
 
 const PERIOD_TICKETS_CONFIG = {
     counties: [],
@@ -35,6 +56,47 @@ const SINGLE_TICKETS_CONFIG = {
     ],
 }
 
+const TRAFIKKPAKKE_1 = [
+    'GOA:Line:50', // Sørtoget Region
+    'GOA:Line:53', // Sørtoget Lokal
+    'GOA:Line:59', // Jærbanen
+]
+
+const TRAFIKKPAKKE_2 = [
+    'SJN:Line:79',
+    'SJN:Line:71',
+    'SJN:Line:72',
+    'SJN:Line:22',
+    'SJN:Line:21',
+    'SJN:Line:26',
+    'SJN:Line:25',
+]
+
+const TRAFIKKPAKKE_3 = [
+    'VYG:Line:43', // Arnalokalen
+    'VYG:Line:41', // Bergensbanen
+    'VYG:Line:45', // Vossebanen
+]
+
+const TRAFIKKPAKKE_4 = [
+    'L1', // Asker/Spikkestad - Lillestrøm
+    'L2', // Skøyen/Stabekk - Ski
+    'L21', // Stabekk - Moss
+    'L22', // Skøyen - Mysen/Rakkestad
+    'R20', // Oslo S - Halden
+    'L3', // Oslo S - Jaren
+    'R30', // Oslo S - Gjøvik
+]
+
+const TRAFIKKPAKKE_5 = [
+    'L12', // Kongsberg - Eidsvoll
+    'L13', // Drammen - Dal
+    'L14', // Asker - Kongsvinger
+    'R10', // Drammen - Lillehammer
+    'R11', // Skien - Eidsvoll
+    'L52', // Notodden - Porsgrunn
+]
+
 type TariffZoneProps = {
     id: string
     name: string
@@ -53,24 +115,24 @@ type CountyProps = {
 
 type County = Feature<MultiPolygon, CountyProps>
 
+type LineProps = {
+    id: string
+    publicCode: string
+}
+
 async function main() {
     try {
-        const tariffZones: FeatureCollection<
-            Polygon,
-            TariffZoneProps
-        > = JSON.parse(
-            await readFile('data/tariffZones.json', {
-                encoding: 'utf-8',
-            }),
-        )
-        const counties: FeatureCollection<
-            MultiPolygon,
-            CountyProps
-        > = JSON.parse(
-            await readFile('data/counties.json', {
-                encoding: 'utf-8',
-            }),
-        )
+        const tariffZones = await readJson<
+            FeatureCollection<Polygon, TariffZoneProps>
+        >('data/tariffZones.json')
+
+        const counties = await readJson<
+            FeatureCollection<MultiPolygon, CountyProps>
+        >('data/counties.json')
+
+        const lines = await readJson<
+            FeatureCollection<MultiLineString, LineProps>
+        >('data/lines.json')
 
         const singleAreas: Array<County | TariffZone> = [
             ...counties.features.filter((county) =>
@@ -98,26 +160,54 @@ async function main() {
             ),
         ]
 
+        const trafikkpakke1 = lines.features.filter((line) =>
+            TRAFIKKPAKKE_1.some((id) => id === line.properties.id),
+        )
+
+        const trafikkpakke2 = lines.features.filter((line) =>
+            TRAFIKKPAKKE_2.some((id) => id === line.properties.id),
+        )
+
+        const trafikkpakke3 = lines.features.filter((line) =>
+            TRAFIKKPAKKE_3.some((id) => id === line.properties.id),
+        )
+
+        const trafikkpakke4 = lines.features.filter((line) =>
+            TRAFIKKPAKKE_4.some((code) => code === line.properties.publicCode),
+        )
+
+        const trafikkpakke5 = lines.features.filter((line) =>
+            TRAFIKKPAKKE_5.some((code) => code === line.properties.publicCode),
+        )
+
         await Promise.all([
-            writeFile(
+            writeJson(
                 'public/singleTickets.json',
-                JSON.stringify({
-                    type: 'FeatureCollection',
-                    features: singleAreas,
-                }),
-                {
-                    encoding: 'utf-8',
-                },
+                featureCollection(singleAreas),
             ),
-            writeFile(
+            writeJson(
                 'public/periodTickets.json',
-                JSON.stringify({
-                    type: 'FeatureCollection',
-                    features: periodAreas,
-                }),
-                {
-                    encoding: 'utf-8',
-                },
+                featureCollection(periodAreas),
+            ),
+            writeJson(
+                'data/trafikkpakke1.json',
+                featureCollection(trafikkpakke1),
+            ),
+            writeJson(
+                'data/trafikkpakke2.json',
+                featureCollection(trafikkpakke2),
+            ),
+            writeJson(
+                'data/trafikkpakke3.json',
+                featureCollection(trafikkpakke3),
+            ),
+            writeJson(
+                'data/trafikkpakke4.json',
+                featureCollection(trafikkpakke4),
+            ),
+            writeJson(
+                'data/trafikkpakke5.json',
+                featureCollection(trafikkpakke5),
             ),
         ])
     } catch (error) {
